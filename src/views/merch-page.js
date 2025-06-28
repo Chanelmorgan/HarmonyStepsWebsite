5ac2fd1135c49847931b734e5b231aa1a8b5e252
Chanel Morgan <chanelmorgan05@icloud.com>
2025-06-28 16:27:07 +0100

    Update merch-page.js

--- src/views/merch-page.js
+++ src/views/merch-page.js
@@ -110,7 +110,7 @@ const MerchPage = () => {
           <p className="merch-item-description"></p>
           <p className="merch-item-price">£16.50</p>
           {/* Order Here button using mailto */}
-          <a className="add-to-cart-button" href="" >Order Here</a>
+          <a className="add-to-cart-button" href="https://buy.stripe.com/cNi8wH3Ut4fs4Eo5Ch77O01" >Order Here</a>
         </div>
 
          <div className="merch-item">
