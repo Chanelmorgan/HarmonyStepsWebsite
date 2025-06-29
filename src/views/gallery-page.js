02a1834ea1852f9228768c84e073d91f32a6d4ad
Chanel Morgan <chanelmorgan05@icloud.com>
2025-06-29 13:03:44 +0100

    update photo

--- src/views/gallery-page.js
+++ src/views/gallery-page.js
@@ -43,6 +43,7 @@ const GalleryPage = (props) => {
     { src: '/s16.png', alt: 'Image 35' },
     { src: '/s17.JPG', alt: 'Image 36' },
     { src: '/s18.JPG', alt: 'Image 37' },
+    { src: '/new_1.JPG', alt: 'Image 38' },
   
 
   
